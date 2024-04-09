44b5ac2ae76592075ef29ad975c26a4761a6f840
GoldSpaghetti <alan.chen.2k6@gmail.com>
2024-04-09 17:50:08 +0800

    added claw commands

--- src/arduino.ino
+++ src/arduino.ino
@@ -2,11 +2,15 @@
 
 const int PINS[] = {5, 6, 9, 10, 11, 4};
 Servo THRUSTERS[6] {};
+const int CLAWPINS[] = {12, 13};
+Servo CLAWS[2]{};
 
 void setup() {
     Serial.begin(115200);
 
     for (int i = 0; i < 6; i++) THRUSTERS[i].attach(PINS[i]);
+
+    for (int i = 0; i < 2; i++) CLAWS[i].attach(CLAWPINS[i]);
 }
 
 void loop() {
@@ -22,9 +26,14 @@ void loop() {
     } cmd;
     Serial.readBytes(cmd.buff, 13);
 
-    if (cmd.cmd_byte != 0x69) return;
-
+    if (cmd.cmd_byte == 0x69){
     for (int i = 0; i < 6; i++)
-        THRUSTERS[i].writeMicroseconds(cmd.thrusters[i]);
+          THRUSTERS[i].writeMicroseconds(cmd.thrusters[i]);
+    }
+    if (cmd.cmd_byte == 0x50){
+      CLAWS[0].writeMicroseconds(cmd.thrusters[0]);
+    }
+    if (cmd.cmd_byte == 0x40){
+      CLAWS[1].writeMicroseconds(cmd.thrusters[0]);
+    }
 }
-
