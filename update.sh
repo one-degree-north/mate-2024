be8c0c1014780cbbb560b0cf490ef7e120bd867b
chickwensrule <chickwensrule@outlook.com>
2024-04-11 00:45:18 +0800

    update sh

--- update.sh
+++ update.sh
@@ -1,7 +1,17 @@
 rm -rf build
 git stash
 git pull
-cmake -B build -G Ninja .
-cd build
-read -p "server/client: " resp
-ninja $resp
+
+read -p "Install server or client (s/c)? " resp
+case $resp in
+    s )
+        mkdir build
+        cd build
+        cmake ..
+        make server
+    c )
+        cmake -B build -G Ninja .
+        cd build
+	ninja client
+    * ) echo "Invalid response.";;
+esac
