d008921bb6489ea97139494e81c566e820dcbdb4
chickwensrule <chickwensrule@outlook.com>
2024-04-12 23:28:13 +0800

    better trust

--- update.sh
+++ update.sh
@@ -2,5 +2,16 @@ rm -rf build
 git stash
 git pull
 
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release .
-cmake --build build
+read -p "Install server or client (s/c)? " resp
+case $resp in
+    s )
+        mkdir build
+        cd build
+        cmake ..
+        make server ;;
+    c )
+        cmake -B build -G Ninja .
+        cd build
+	ninja client ;;
+    * ) echo "Invalid response." ;;
+esac
